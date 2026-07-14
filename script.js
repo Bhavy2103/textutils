8e8cbaf64a08bb8681605d47b6bc7f39942ae6df
Bhavy Shah <shahbhavy260@gmail.com>
2026-07-14 19:00:19 +0530

    Enhance text manipulation features and update HTML structure

--- script.js
+++ script.js
@@ -2,31 +2,88 @@ const text = document.getElementById("text");
 
 text.addEventListener("input", updateCount);
 
-function upperCase(){
+function upperCase() {
     text.value = text.value.toUpperCase();
     updateCount();
 }
 
-function lowerCase(){
+function lowerCase() {
     text.value = text.value.toLowerCase();
     updateCount();
 }
 
-function capitalizeText(){
-    text.value = text.value
-    .toLowerCase()
-    .split(" ")
-    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-    .join(" ");
+function italic() {
+    if (text.style.fontStyle === "italic") {
+        text.style.fontStyle = "normal";
+    } else {
+        text.style.fontStyle = "italic";
+    }
+    updateCount();
+}
+function Underline() {
+    if (text.style.textDecoration === "Underline") {
+        text.style.textDecoration = "normal";
+    } else {
+        text.style.textDecoration = "Underline";
+    }
     updateCount();
 }
 
-function clearText(){
+function H1() {
+    if (text.style.fontSize === "40px") {
+        text.style.fontSize = "normal"
+    }
+    else {
+        text.style.fontSize = "40px"
+    }
+    updateCount();
+}
+
+function Reverse() {
+    text.value = text.value.split("").reverse().join("")
+    updateCount();
+}
+
+function Copy() {
+    navigator.clipboard.writeText(text.value)
+        ? text.style.color = "Green"
+        : navigator.clipboard.writeText(text.value);
+    updateCount();
+}
+
+async function Past() {
+    text.value = await navigator.clipboard.readText()
+    text.style.color = "Green"
+    updateCount();
+}
+function bold() {
+    if (text.style.fontWeight === "bold") {
+        text.style.fontWeight = "normal";
+    }
+    else {
+        text.style.fontWeight = "bold";
+    }
+
+    updateCount();
+}
+
+
+
+function capitalizeText() {
+    text.value = text.value
+        .toLowerCase()
+        .split(" ")
+        .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(" ");
+    updateCount();
+}
+
+function clearText() {
     text.value = "";
     updateCount();
 }
 
-function updateCount(){
+function updateCount() {
     document.getElementById("chars").textContent = text.value.length;
 
     const words = text.value.trim() === ""
@@ -34,4 +91,7 @@ function updateCount(){
         : text.value.trim().split(/\s+/).length;
 
     document.getElementById("words").textContent = words;
+
+    const Line = text.value.trim() === "" ? 0 : text.value.trim().split("\n").length;
+    document.getElementById("Line").textContent = Line;
 }
